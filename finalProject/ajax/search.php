bf0c7829d5956e78fb5727ce15b30ded178fdcfc
Yudha Yunanto Putra <61044350+yudhayunantop@users.noreply.github.com>
2020-05-27 16:19:56 +0700

    Update search.php

--- finalProject/ajax/search.php
+++ finalProject/ajax/search.php
@@ -3,23 +3,19 @@ require '../functions.php';
 $keyword = $_GET["keyword"]; 
 $query = "SELECT * FROM iklan 
             WHERE
-            nama LIKE '%$keyword%' OR
-            noTelp LIKE '%$keyword%' OR
-            alamat LIKE '%$keyword%'
+            nama LIKE '%$keyword%'
         ";
 $iklan = query($query);
-
 ?>
-
-<div id="container">		
-	<?php foreach ($iklan as $row):?>
-	<article class="card" id="iklan">
-		<h3><?php echo $row["nama"];?></h3>
-			<img src="img/<?php echo $row["gambar"];?>" width="500">
-			<h4><?php echo $row["noTelp"];?></h4>
-			<p><?php echo $row["email"];?></p>
-			<p><?php echo $row["alamat"];?></p>
-			<p><?php echo $row["deskripsi"];?></p>
-	</article>
-	<?php endforeach;?>
-</div>
+		<div id="container">
+			<?php foreach ($iklan as $row):?>
+				<div id="content" class="aman">		
+					<article id="iklan">
+						<a href="penjelasan.php?id=<?php echo $row["id"];?>" target='_blank'>
+							<h3><?php echo $row["nama"];?></h3> 
+						</a>
+							<img src="img/<?php echo $row["gambar"];?>" width="200" height="150" alt="<?php echo $row["nama"];?>">
+					</article>
+				</div>
+			<?php endforeach;?>
+		</div>
