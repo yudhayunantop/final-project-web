866072333ce583ef6051f231e9672a7aed7d2426
Yudha Yunanto Putra <61044350+yudhayunantop@users.noreply.github.com>
2020-05-27 16:19:35 +0700

    perbaikan tampilan

--- finalProject/index.php
+++ finalProject/index.php
@@ -25,46 +25,21 @@ $iklan = query("SELECT * FROM iklan;");
 		</nav>
 	</header>
 	<main>
-	
-		<div id="content">
-			<form action="" method="post">
-				<input class="cari" type="text" name="keyword" size="40" autofocus placeholder="Masukkan keyword pencarian..." autocomplete="off" id="keyword">
-			</form>
-		<div id="container">		
+		<form action="" method="post">
+			<input class="cari" type="text" name="keyword" size="40" autofocus placeholder="Masukkan keyword pencarian..." autocomplete="off" id="keyword">
+		</form>
+		<div id="container">
 			<?php foreach ($iklan as $row):?>
-			<article class="card" id="iklan">
-				<h3><?php echo $row["nama"];?></h3>
-					<img src="img/<?php echo $row["gambar"];?>" width="500">
-					<h4><?php echo $row["noTelp"];?></h4>
-					<p><?php echo $row["email"];?></p>
-					<p><?php echo $row["alamat"];?></p>
-					<p><?php echo $row["deskripsi"];?></p>
-			</article>
+				<div id="content" class="aman">		
+					<article id="iklan">
+						<a href="penjelasan.php?id=<?php echo $row["id"];?>" target='_blank'>
+							<h3><?php echo $row["nama"];?></h3> 
+						</a>
+							<img src="img/<?php echo $row["gambar"];?>" width="200" height="150" alt="<?php echo $row["nama"];?>">
+					</article>
+				</div>
 			<?php endforeach;?>
 		</div>
-		</div>
-		<aside>
-			<article class="profile card">
-			<header>
-			<h2>Tentang Developper</h2>
-			<figure>
-						<img src="assets/profil.png">
-					</figure>
-				</header>
-				<section>
-					<table>
-						<tr>
-							<th>Nama</th>
-							<td>Yudha & Hasan</td>
-						</tr>
-						<tr>
-							<th>Universitas</th>
-							<td>UPN "Veteran" Jawa Timur</td>
-						</tr>
-					</table>
-				</section>
-			</article>
-		</aside>
 	</main>
 	<footer>
 		<p class="fontputih">Copyright &copy;yudhayp2020</p>
